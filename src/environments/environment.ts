02e21235132fd4a5d14aabd47d57a1ad80544245
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-04-15 20:55:50 +0300

    ft: hide API KEY in environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: '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'
 };
 
 /*
