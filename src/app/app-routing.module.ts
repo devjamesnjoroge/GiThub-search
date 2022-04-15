027f3534d2108d03bdc2009476c48f9ba03c09a0
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-04-15 20:14:47 +0300

    ft: add my profile template and its route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'devjamesnjoroge', component: MyProfileComponent },
+  { path: '', redirectTo: '/devjamesnjoroge', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
