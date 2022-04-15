b080788b2091099b7d01e505966eb8a366dbe174
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-04-15 21:00:43 +0300

    ft: add GET request in service component

--- src/app/gitHub/git-hub.service.ts
+++ src/app/gitHub/git-hub.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { GitHubUser } from './git-hub-user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitHubService {
 
-
-
   constructor(private http: HttpClient) { }
 
   getGitHubProfile() {
-
+    return this.http.get<GitHubUser>(`https://api.github.com/users/devjamesnjoroge?${environment.API_KEY}`)
   }
 }
